ab4fb8f29a8dcf6d465b0e20890f81aff3edd52c
corrodedHash <corrodedHash@users.noreply.github.com>
2018-06-05 02:44:25 +0200

    Made List Iterator more generic

--- include/DancingLinks/RowIterator.hpp
+++ include/DancingLinks/RowIterator.hpp
@@ -2,51 +2,57 @@
 #include "DancingLinks/Node.hpp"
 
 namespace DancingLinks {
-class RowExcludingRightIterator {
+template <Node* Node::*Next, bool Excluding>
+class LineIteratorImpl;
+
+template <Node* Node::*Next>
+class LineIteratorImpl<Next, true> {
 private:
   Node* StartNode;
 
 public:
+  using iterator = LineIteratorImpl<Next, true>;
   using iterator_category = std::forward_iterator_tag;
   using value_type = Node;
   using pointer = Node*;
   using reference = Node&;
   using difference_type = long;
 
-  RowExcludingRightIterator(Node* node) : StartNode(node->Right) {}
+  LineIteratorImpl(Node* node) : StartNode(node->*Next) {}
 
-  RowExcludingRightIterator& operator++() {
-    StartNode = StartNode->Right;
+  iterator& operator++() {
+    StartNode = StartNode->*Next;
     return *this;
   }
-  RowExcludingRightIterator operator++(int) {
-    RowExcludingRightIterator buffer = *this;
-    StartNode = StartNode->Right;
+  iterator operator++(int) {
+    iterator buffer = *this;
+    StartNode = StartNode->*Next;
     return buffer;
   }
 
   pointer operator->() const { return StartNode; }
   reference operator*() const { return *StartNode; }
 
-  bool operator==(const RowExcludingRightIterator& other) const {
+  bool operator==(const iterator& other) const {
     return other.StartNode == StartNode;
   }
-  bool operator!=(const RowExcludingRightIterator& other) const {
-    return !(*this == other);
-  }
+  bool operator!=(const iterator& other) const { return !(*this == other); }
 };
+
+using RowExcludingRightIterator = LineIteratorImpl<&Node::Right, true>;
+using ColumnExcludingDownIterator = LineIteratorImpl<&Node::Down, true>;
 
 } // namespace DancingLinks
 
-template <>
-struct std::iterator_traits<DancingLinks::RowExcludingRightIterator> {
+template <auto A, auto B>
+struct std::iterator_traits<DancingLinks::LineIteratorImpl<A, B>> {
   using iterator_category =
-      DancingLinks::RowExcludingRightIterator::iterator_category;
-  using value_type = DancingLinks::RowExcludingRightIterator::value_type;
-  using pointer = DancingLinks::RowExcludingRightIterator::pointer;
-  using reference = DancingLinks::RowExcludingRightIterator::reference;
+      typename DancingLinks::LineIteratorImpl<A, B>::iterator_category;
+  using value_type = typename DancingLinks::LineIteratorImpl<A, B>::value_type;
+  using pointer = typename DancingLinks::LineIteratorImpl<A, B>::pointer;
+  using reference = typename DancingLinks::LineIteratorImpl<A, B>::reference;
   using difference_type =
-      DancingLinks::RowExcludingRightIterator::difference_type;
+      typename DancingLinks::LineIteratorImpl<A, B>::difference_type;
 };
 
 namespace DancingLinks {
