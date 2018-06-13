97d46bcf7a803db953725f51e2bebdaafb044996
corrodedHash <corrodedHash@users.noreply.github.com>
2018-06-13 23:57:23 +0200

    Improved build system

--- cmake/ExternalCatch2.cmake
+++ cmake/ExternalCatch2.cmake
@@ -1,24 +1,30 @@
-include(ExternalProject)
-find_package(Git REQUIRED)
+find_package(Catch2)
 
-set(CATCH2_EXTERNAL_ROOT ${CMAKE_BINARY_DIR}/externals/catch2)
-set(CATCH2_EXTERNAL_REPO "https://github.com/philsquared/Catch.git")
+if (NOT Catch2_FOUND)
+  include(ExternalProject)
+  find_package(Git REQUIRED)
 
-ExternalProject_Add(
+  set(CATCH2_EXTERNAL_ROOT ${CMAKE_BINARY_DIR}/externals/catch2)
+  set(CATCH2_EXTERNAL_REPO "https://github.com/philsquared/Catch.git")
+
+  ExternalProject_Add(
     extlib_catch2
     PREFIX ${CATCH2_EXTERNAL_ROOT}
-    GIT_REPOSITORY ${CATCH2_EXTERNAL_REPO} 
+    GIT_REPOSITORY ${CATCH2_EXTERNAL_REPO}
     TIMEOUT 10
     #UPDATE_COMMAND ${GIT_EXECUTABLE} pull
-    UPDATE_COMMAND "" 
+    UPDATE_COMMAND ""
     CONFIGURE_COMMAND ""
     BUILD_COMMAND ""
     INSTALL_COMMAND ""
     LOG_DOWNLOAD ON
-   )
+    )
 
-# Expose required variable (CATCH_INCLUDE_DIR) to parent scope
-ExternalProject_Get_Property(extlib_catch2 source_dir)
-add_library(Catch2 INTERFACE)
-add_dependencies(Catch2 extlib_catch2)
-target_include_directories(Catch2 SYSTEM INTERFACE ${source_dir}/single_include)
+  # Expose required variable (CATCH_INCLUDE_DIR) to parent scope
+  ExternalProject_Get_Property(extlib_catch2 source_dir)
+  add_library(Catch2::Catch INTERFACE IMPORTED)
+  add_dependencies(Catch2::Catch extlib_catch2)
+  target_include_directories(Catch2::Catch SYSTEM INTERFACE ${source_dir}/single_include)
+else()
+  message(STATUS "Using system Catch2")
+endif()
--- cmake/ExternalGoogleBenchmarks.cmake
+++ cmake/ExternalGoogleBenchmarks.cmake
@@ -1,44 +1,50 @@
-include(ExternalProject)
-find_package(Git REQUIRED)
+find_package(benchmark)
 
-set(GOOGLEBENCH_EXTERNAL_ROOT ${CMAKE_BINARY_DIR}/externals/googlebench)
-set(GOOGLEBENCH_EXTERNAL_REPO "https://github.com/google/benchmark.git")
+if (NOT benchmark_FOUND)
+  include(ExternalProject)
+  find_package(Git REQUIRED)
 
-ExternalProject_ADD(
-  #--External-project-name------
-  extlib_googlebench
-  #--Core-directories-----------
-  PREFIX             ${GOOGLEBENCH_EXTERNAL_ROOT}
-  #--Download step--------------
-  GIT_REPOSITORY     ${GOOGLEBENCH_EXTERNAL_REPO}
-  GIT_SHALLOW 1
-  TIMEOUT            10
-  LOG_DOWNLOAD       ON
-  #--Update step----------
-  #UPDATE_COMMAND     ${GIT_EXECUTABLE} pull
-  UPDATE_COMMAND ""
-  #--Patch step----------
-  # PATCH_COMMAND sh -c "cp <SOURCE_DIR>/scripts/CMakeLists.txt <SOURCE_DIR>"
-  #--Configure step-------------
-  CMAKE_ARGS -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} -DBENCHMARK_ENABLE_TESTING=OFF -DCMAKE_INSTALL_PREFIX:PATH=<INSTALL_DIR>
-  LOG_CONFIGURE ON
-  #--Build step-----------------
-  BUILD_COMMAND ${CMAKE_MAKE_PROGRAM}
-  LOG_BUILD ON
-  #--Install step---------------
-  # INSTALL_COMMAND    ""
-  # INSTALL_DIR ${CMAKE_BINARY_DIR}/
-  #--Install step---------------
-  # INSTALL_COMMAND    ""
-)
+  set(GOOGLEBENCH_EXTERNAL_ROOT ${CMAKE_BINARY_DIR}/externals/googlebench)
+  set(GOOGLEBENCH_EXTERNAL_REPO "https://github.com/google/benchmark.git")
 
-ExternalProject_Get_Property(extlib_googlebench INSTALL_DIR)
-# This is pure sadness
-# The problem is that cmake cannot attribute the libbenchmark.a to the external project
-# So we have to create a stub, otherwise GoogleBench cannot find a rule to create it.
+  ExternalProject_ADD(
+    #--External-project-name------
+    extlib_googlebench
+    #--Core-directories-----------
+    PREFIX             ${GOOGLEBENCH_EXTERNAL_ROOT}
+    #--Download step--------------
+    GIT_REPOSITORY     ${GOOGLEBENCH_EXTERNAL_REPO}
+    GIT_SHALLOW 1
+    TIMEOUT            10
+    LOG_DOWNLOAD       ON
+    #--Update step----------
+    #UPDATE_COMMAND     ${GIT_EXECUTABLE} pull
+    UPDATE_COMMAND ""
+    #--Patch step----------
+    # PATCH_COMMAND sh -c "cp <SOURCE_DIR>/scripts/CMakeLists.txt <SOURCE_DIR>"
+    #--Configure step-------------
+    CMAKE_ARGS -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} -DBENCHMARK_ENABLE_TESTING=OFF -DCMAKE_INSTALL_PREFIX:PATH=<INSTALL_DIR>
+    LOG_CONFIGURE ON
+    #--Build step-----------------
+    BUILD_COMMAND ${CMAKE_MAKE_PROGRAM}
+    LOG_BUILD ON
+    #--Install step---------------
+    # INSTALL_COMMAND    ""
+    # INSTALL_DIR ${CMAKE_BINARY_DIR}/
+    #--Install step---------------
+    # INSTALL_COMMAND    ""
+    )
 
-file(APPEND "${INSTALL_DIR}/lib/libbenchmark.a" "")
-add_library(GoogleBench INTERFACE)
-add_dependencies(GoogleBench extlib_googlebench)
-target_link_libraries(GoogleBench INTERFACE "${INSTALL_DIR}/lib/libbenchmark.a" pthread)
-target_include_directories(GoogleBench INTERFACE "${INSTALL_DIR}/include")
+  ExternalProject_Get_Property(extlib_googlebench INSTALL_DIR)
+  # This is pure sadness
+  # The problem is that cmake cannot attribute the libbenchmark.a to the external project
+  # So we have to create a stub, otherwise GoogleBench cannot find a rule to create it.
+
+  file(APPEND "${INSTALL_DIR}/lib/libbenchmark.a" "")
+  add_library(benchmark::benchmark INTERFACE IMPORTED)
+  add_dependencies(benchmark::benchmark extlib_googlebench)
+  target_link_libraries(benchmark::benchmark INTERFACE "${INSTALL_DIR}/lib/libbenchmark.a" pthread)
+  target_include_directories(benchmark::benchmark INTERFACE "${INSTALL_DIR}/include")
+else()
+  message(STATUS "Using system google benchmark")
+endif()
