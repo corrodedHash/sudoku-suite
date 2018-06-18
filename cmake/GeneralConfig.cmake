37c3868c95faf5a3fe1e12cdf812a515a0b138b8
corrodedHash <corrodedHash@users.noreply.github.com>
2018-06-18 11:40:35 +0200

    Reformatted build system

--- cmake/GeneralConfig.cmake
+++ cmake/GeneralConfig.cmake
@@ -16,73 +16,80 @@ else()
 
   target_compile_options(GeneralConfig INTERFACE "-fno-omit-frame-pointer")
 
+  set(SANITIZER_LIST "")
+  if(${${PROJECT_NAME}_SANITIZER_ADDRESS})
+    list(APPEND SANITIZER_LIST "address")
+    target_compile_options(GeneralConfig INTERFACE
+      "-fsanitize-address-use-after-scope"
+      "-fno-optimize-sibling-calls")
+    target_link_libraries(GeneralConfig INTERFACE
+      "-fsanitize-address-use-after-scope"
+      "-fno-optimize-sibling-calls")
+  endif()
+
+  if(${${PROJECT_NAME}_SANITIZER_UB})
+    list(APPEND SANITIZER_LIST "undefined")
+  endif()
+
+  if(${${PROJECT_NAME}_SANITIZER_MEMORY})
+    include("ExternalMsanLibcxx")
+    target_link_libraries(GeneralConfig INTERFACE libcxx::msanlibcxx)
+
+    list(APPEND SANITIZER_LIST "memory")
+    target_compile_options(GeneralConfig INTERFACE
+      "-fsanitize-memory-track-origins")
+    target_link_libraries(GeneralConfig INTERFACE
+      "-fsanitize-memory-track-origins")
+  endif()
+
+  if (${${PROJECT_NAME}_SANITIZER_CFI})
+    # Segfaults with 'illegal hardware instruction'
+    list(APPEND SANITIZER_LIST "cfi")
+    target_compile_options(GeneralConfig INTERFACE
+      "-fvisibility=hidden")
+  endif()
+
+  if(${PROJECT_NAME}_COVERAGE)
+
+    target_compile_options(GeneralConfig INTERFACE
+      "-fprofile-instr-generate" "-fcoverage-mapping")
+    target_link_libraries(GeneralConfig INTERFACE
+      "-fprofile-instr-generate" "-fcoverage-mapping")
+
+  endif()
+
+  if (${PROJECT_NAME}_FUZZER)
+
+    list(APPEND SANITIZER_LIST "fuzzer-no-link")
+    string(REPLACE ";" "," SANITIZER_STRING "${SANITIZER_LIST}" )
+    target_compile_options(GeneralConfig INTERFACE
+      "-fsanitize=${SANITIZER_STRING}")
+    target_link_libraries(GeneralConfig INTERFACE
+      "-fsanitize=${SANITIZER_STRING}")
+
+    add_library(FuzzerConfig INTERFACE)
+    target_link_libraries(FuzzerConfig INTERFACE GeneralConfig)
+
+    list(REMOVE_ITEM SANITIZER_LIST "fuzzer-no-link")
+    list(APPEND SANITIZER_LIST "fuzzer")
+    string(REPLACE ";" "," SANITIZER_STRING "${SANITIZER_LIST}" )
+    target_compile_options(FuzzerConfig INTERFACE
+      "-fsanitize=${SANITIZER_STRING}")
+    target_link_libraries(FuzzerConfig INTERFACE
+      "-fsanitize=${SANITIZER_STRING}")
+
+  else()
+
+    string(REPLACE ";" "," SANITIZER_STRING "${SANITIZER_LIST}" )
+    target_compile_options(GeneralConfig INTERFACE
+      "-fsanitize=${SANITIZER_STRING}")
+    target_link_libraries(GeneralConfig INTERFACE
+      "-fsanitize=${SANITIZER_STRING}")
+
+  endif()
+
+
   if("${CMAKE_BUILD_TYPE}" STREQUAL "DEBUG")
-
-    set(SANITIZER_LIST "")
-    if(${${PROJECT_NAME}_SANITIZER_ADDRESS})
-      list(APPEND SANITIZER_LIST "address")
-      target_compile_options(GeneralConfig INTERFACE
-        "-fsanitize-address-use-after-scope"
-        "-fno-optimize-sibling-calls")
-      target_link_libraries(GeneralConfig INTERFACE
-        "-fsanitize-address-use-after-scope"
-        "-fno-optimize-sibling-calls")
-    endif()
-
-    if(${${PROJECT_NAME}_SANITIZER_UB})
-      list(APPEND SANITIZER_LIST "undefined")
-    endif()
-
-    if(${${PROJECT_NAME}_SANITIZER_MEMORY})
-      include("ExternalMsanLibcxx")
-      target_link_libraries(GeneralConfig INTERFACE libcxx::msanlibcxx)
-
-      list(APPEND SANITIZER_LIST "memory")
-      target_compile_options(GeneralConfig INTERFACE
-        "-fsanitize-memory-track-origins")
-      target_link_libraries(GeneralConfig INTERFACE
-        "-fsanitize-memory-track-origins")
-    endif()
-
-    if(${PROJECT_NAME}_COVERAGE)
-
-      target_compile_options(GeneralConfig INTERFACE
-        "-fprofile-instr-generate" "-fcoverage-mapping")
-      target_link_libraries(GeneralConfig INTERFACE
-        "-fprofile-instr-generate" "-fcoverage-mapping")
-
-    endif()
-
-    if (${PROJECT_NAME}_FUZZER)
-
-      list(APPEND SANITIZER_LIST "fuzzer-no-link")
-      string(REPLACE ";" "," SANITIZER_STRING "${SANITIZER_LIST}" )
-      target_compile_options(GeneralConfig INTERFACE
-        "-fsanitize=${SANITIZER_STRING}")
-      target_link_libraries(GeneralConfig INTERFACE
-        "-fsanitize=${SANITIZER_STRING}")
-
-      add_library(FuzzerConfig INTERFACE)
-      target_link_libraries(FuzzerConfig INTERFACE GeneralConfig)
-
-      list(REMOVE_ITEM SANITIZER_LIST "fuzzer-no-link")
-      list(APPEND SANITIZER_LIST "fuzzer")
-      string(REPLACE ";" "," SANITIZER_STRING "${SANITIZER_LIST}" )
-      target_compile_options(FuzzerConfig INTERFACE
-        "-fsanitize=${SANITIZER_STRING}")
-      target_link_libraries(FuzzerConfig INTERFACE
-        "-fsanitize=${SANITIZER_STRING}")
-
-    else()
-
-      string(REPLACE ";" "," SANITIZER_STRING "${SANITIZER_LIST}" )
-      target_compile_options(GeneralConfig INTERFACE
-        "-fsanitize=${SANITIZER_STRING}")
-      target_link_libraries(GeneralConfig INTERFACE
-        "-fsanitize=${SANITIZER_STRING}")
-
-    endif()
-
   elseif("${CMAKE_BUILD_TYPE}" STREQUAL "RELEASE")
 
     message(STATUS "Compiling with release")
